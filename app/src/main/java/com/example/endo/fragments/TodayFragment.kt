5c81886a0a76be26be591b8fe1a406458a28b1b9
Azamat <azamat.nazar99@gmail.com>
2021-12-17 17:27:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/endo/fragments/TodayFragment.kt
@@ -0,0 +1,10 @@
+package com.example.endo.fragments
+
+import com.example.core.base.BaseFragment
+import com.example.endo.databinding.FragmentTodayBinding
+
+
+class TodayFragment : BaseFragment<FragmentTodayBinding>(FragmentTodayBinding::inflate) {
+
+
+}
