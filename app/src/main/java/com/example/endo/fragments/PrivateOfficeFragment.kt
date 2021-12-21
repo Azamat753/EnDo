602f791a94e8d84f5f90b963093e7e39e62ef345
Azamat <azamat.nazar99@gmail.com>
2021-12-21 18:01:15 +0600

    21.12.21 #2

---
+++ app/src/main/java/com/example/endo/fragments/PrivateOfficeFragment.kt
@@ -0,0 +1,23 @@
+package com.example.endo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.core.base.BaseFragment
+import com.example.endo.R
+import com.example.endo.databinding.FragmentPrivateOfficeBinding
+
+class PrivateOfficeFragment : BaseFragment<FragmentPrivateOfficeBinding>(FragmentPrivateOfficeBinding::inflate) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    override fun initClickers() {
+
+    }
+
+
+}
