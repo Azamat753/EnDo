746cced14d3416dc2c2e4858dc7897a5e79ff38e
Clar1fy <iamonmydoctorpath@gmail.com>
2022-03-12 23:19:27 +0600

    add listeningflow package that includes its fragments, set up proper navigation

---
+++ app/src/main/java/com/example/endo/fragments/listeningflow/MusicFragment.kt
@@ -0,0 +1,22 @@
+package com.example.endo.fragments.listeningflow
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.core.base.BaseFragment
+import com.example.endo.R
+import com.example.endo.databinding.FragmentMusicBinding
+
+
+class MusicFragment : BaseFragment<FragmentMusicBinding>(FragmentMusicBinding::inflate) {
+    override fun initObserver() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initClickers() {
+        TODO("Not yet implemented")
+    }
+
+}
