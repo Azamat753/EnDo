6ca6dbf8bddb6c516f9233015471ac4045db452c
Clar1fy <iamonmydoctorpath@gmail.com>
2022-03-15 20:48:33 +0600

    sleep\

--- app/src/main/java/com/example/endo/fragments/listeningflow/AudioTestFragment.kt
+++ app/src/main/java/com/example/endo/fragments/listeningflow/AudioTestFragment.kt
@@ -1,6 +1,8 @@
 package com.example.endo.fragments.listeningflow
 
 import android.graphics.RenderEffect
+import android.view.MotionEvent
+import android.view.View
 import com.example.core.base.BaseFragment
 import com.example.endo.databinding.FragmentAudioTestBinding
 
@@ -13,11 +15,6 @@ class AudioTestFragment :
     }
 
     override fun initClickers() = with(binding) {
-        btnAnswer1.setOnClickListener {
-            btnAnswer2.background.alpha = 100
-            btnAnswer3.background.alpha = 100
-            btnAnswer4.background.alpha = 100
-        }
         btnAnswer2.setOnClickListener {
 
 
@@ -28,6 +25,17 @@ class AudioTestFragment :
         btnAnswer4.setOnClickListener {
 
         }
+        btnAnswer1.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+                btnAnswer1.isPressed = true
+                btnAnswer2.background.alpha = 50
+                btnAnswer3.background.alpha = 50
+                btnAnswer4.background.alpha = 50
+
+                return true
+            }
+        })
+
 
 
     }
--- app/src/main/java/com/example/endo/models/QuestionModel.kt
+++ app/src/main/java/com/example/endo/models/QuestionModel.kt
@@ -1,7 +1,15 @@
 package com.example.endo.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+
+
+@Parcelize
 data class QuestionModel(
      var id: Int,
      val question: String
-) {
+): Parcelable {
+
 }
